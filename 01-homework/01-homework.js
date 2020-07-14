43e8ba1a8fdf37e8b0ce55a3dccd7bbda2624166
mdubchak <dubchmar@gmail.com>
2020-07-14 18:43:29 +0300

    created homework 1

---
+++ 01-homework/01-homework.js
@@ -0,0 +1,73 @@
+const dress = 15.678;
+const shirt = 123.965;
+const vest = 90.2345;
+
+let allProducts = [ dress, shirt, vest ];
+
+let maxNumber = Math.max(dress, shirt, vest);
+console.log(maxNumber);
+
+
+let minNumber = Math.min(dress, shirt, vest);
+console.log(minNumber);
+
+
+let sumProducts = 0;
+for ( i = 0; i < allProducts.length; i++) {
+    sumProducts = sumProducts + parseFloat(allProducts[i]);
+}
+console.log(sumProducts);
+
+
+let sumIntegerProducts = 0;
+for ( i = 0; i < allProducts.length; i++) {
+    sumIntegerProducts = sumIntegerProducts + +(parseInt(allProducts[i]));
+}
+
+console.log(sumIntegerProducts);
+
+let devisionSumInteger = sumIntegerProducts / 1000;
+let roundingToHundredths= devisionSumInteger.toFixed(1) * 1000;
+console.log(roundingToHundredths);
+
+
+let boolean = sumIntegerProducts % 2 == 0;
+console.log(boolean);
+
+let customerCash = 500;
+let change = customerCash - sumProducts;
+console.log(change);
+
+
+let avarege = (sumProducts / allProducts.length).toFixed(2);
+console.log(avarege);
+
+
+let discount = Math.random();
+console.log(discount);
+
+let netProfit = (sumProducts * discount - sumProducts / 2).toFixed(2);
+console.log(netProfit);
+
+
+let results = `<p>1. Максимальна ціна: ${maxNumber};</p>
+<p>2. Мінімальна ціна: ${minNumber};</p>
+<p>3. Вартість усіх товарів: ${sumProducts};</p>
+<p>4. Округлена в меншу сторону вартість товарів: ${sumIntegerProducts};</p>
+<p>5. Округлена до сотень вартість товарів: ${roundingToHundredths};</p>
+<p>6. Чи є окргулена вартість товарів (4) парним числом: ${boolean};</p>
+<p>7. Решта: ${change};</p>
+<p>8. Середнє значення: ${avarege};</p>
+<p>9. Чистий прибуток: ${netProfit};</p>
+`;
+
+document.writeln(results);
+
+
+
+
+
+
+
+
+
