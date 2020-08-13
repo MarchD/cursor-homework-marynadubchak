9b64bc801c56835d570578b820314d42b2337f14
mdubchak <dubchmar@gmail.com>
2020-08-13 13:58:57 +0300

    added #HW8 (First part)

---
+++ 08-homework/script.js
@@ -0,0 +1,87 @@
+class Student {
+    constructor(course, university, fullName) {
+        this.university = university;
+        this.course = course;
+        this.fullName = fullName;
+        this.allMarks = [5, 4, 4, 5];
+        this.isDismissed = false;
+    }
+
+    getInfo() {
+        return 'Студент ' + this.course + '-го курсу, ВНЗ: ' + this.university + ', ' + this.fullName; 
+    }
+
+    get Marks() {
+        if(!this.isDismissed) {
+            return this.allMarks;
+        } else {
+            return null;
+        }
+    } 
+
+    set Marks(mark) {
+        if(!this.isDismissed) {
+            return this.allMarks.push(mark);
+        } else {
+            return null;
+        }
+    }
+
+    getAverageMark() {
+        if(this.isDismissed) {
+            return null;
+        } else {
+            let total = 0;
+            return total = this.allMarks.reduce((total, mark) =>  total + mark) / this.allMarks.length;
+        }        
+    }
+
+    dismiss() {
+        return this.isDismissed = true;
+    }
+
+    recover() { 
+        return this.isDismissed = false;
+    }
+}
+
+
+const student = new Student('5', 'ОНАЗ', 'Карина');
+const student2 = new Student('2', 'ОНАЗ', 'Сергій');
+
+console.log(student.getInfo());
+student.Marks = 5;
+console.log(`Оцінки студентки ${student.fullName} з останньою відміткою: ${student.Marks}`); 
+console.log(`Середня оцінка ${student.fullName}: ${student.getAverageMark()}`); 
+
+console.log(`Студент ${student2.getInfo()} подав заяву на виключення: ${student2.dismiss()}`);
+console.log(`Оцінки студента ${student2.fullName}: ${student2.Marks}`);
+console.log(`Середня оцінка студента ${student2.fullName}: ${student2.getAverageMark()}`);
+
+student2.recover();
+console.log(`Студент ${student2.fullName} повернувся до ВНЗ`); 
+console.log(`${student2.fullName} пропустив багато матеріалу та отримав кол`);
+student2.Marks = 1;
+
+console.log(`Оцінки студента ${student2.fullName}: ${student2.Marks}`); 
+console.log(`Середня оцінка студента ${student2.fullName}: ${student2.getAverageMark()}`);
+
+
+/* ---------------------------------------------------------------------------------------- */
+
+// class BudgetStudent extends Student {
+//     getScholarship() {
+//         if(this.getAverageMark() < 4) {
+//             return console.log(`Твій середній ${this.getAverageMark()}, для стипендії має бути вище 4.0`);
+//         } else if (this.getAverageMark === null) {
+//             return console.log(`Студент більше не навчається у ВНЗ`);
+//         } else {
+//             return console.log(`Ви отримали 1400 грн. стипендії`);
+//         }
+//     }
+// }
+
+// const budget = new BudgetStudent('5', 'ОНАЗ', 'Карина');
+// const budget2 = new BudgetStudent('2', 'ОНАЗ', 'Сергій');
+// budget.getScholarship();
+// budget2.getScholarship();
